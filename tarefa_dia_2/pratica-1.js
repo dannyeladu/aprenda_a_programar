2fc490738d51d518b2ce55602e1b0c190642b1f6
Daniela Duarte Silva <dannyeladu@yahoo.com.br>
2022-08-25 02:15:47 -0300

    Meu quarto codigo

---
+++ tarefa_dia_2/pratica-1.js
@@ -0,0 +1,20 @@
+let nome
+nome = "Daniela"
+console.log(nome)
+let email
+email = "dannyeladu@yahoo.com.br"
+console.log(email)
+let profissao
+profissao = "Analista"
+console.log(profissao)
+let idade
+idade = 41
+console.log(idade)
+let lugarDosSonhos
+lugarDosSonhos = "/Dubai"
+console.log(lugarDosSonhos)
+
+
+
+
+
