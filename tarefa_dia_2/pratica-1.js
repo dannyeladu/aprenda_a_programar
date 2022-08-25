8c92f1c15370870798508259b9ef4065709c8d2d
Daniela Duarte Silva <dannyeladu@yahoo.com.br>
2022-08-25 03:11:04 -0300

    Meu quinto codigo

--- tarefa_dia_2/pratica-1.js
+++ tarefa_dia_2/pratica-1.js
@@ -14,7 +14,9 @@ let lugarDosSonhos
 lugarDosSonhos = "/Dubai"
 console.log(lugarDosSonhos)
 
-
+let frase
+frase = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + lugarDosSonhos + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+console.log(frase)
 
 
 
